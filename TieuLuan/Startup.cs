4dcfd386ae1f971450686b0abd2c0674f8a8a7f3
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-08-13 15:01:20 +0700

    Khởi Tạo Project

---
+++ TieuLuan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TieuLuan.Startup))]
+namespace TieuLuan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
