efce208af884ebf6aede7f47214414d2bea23e9d
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-08-17 00:00:04 +0700

    Catagory Complete Admin

---
+++ TieuLuan/Models/CategoryMetaData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TieuLuan.Models
+{
+    [MetadataTypeAttribute(typeof(Category.CategoryMetaData))]
+    public partial class Category
+    {
+        internal sealed class CategoryMetaData
+        {
+            [Display(Name="Danh Mục")]
+            public int CategoryId { get; set; }
+            
+            [Display(Name="Tên Danh Mục")]
+            [Required(ErrorMessage="Tên Danh Mục không được để rỗng")]
+            public string CategoryName { get; set; }
+            [Display(Name="Mô tả")]
+            [Required(ErrorMessage = "Mô tả về danh mục không được để rỗng")]
+            public string CategoryDetails { get; set; }
+    
+        }
+    }
+   
+}
