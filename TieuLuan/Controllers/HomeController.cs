894d83cadfb514523cad07e0bfb55429f4b4989f
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-08-18 16:04:44 +0700

    WS

--- TieuLuan/Controllers/HomeController.cs
+++ TieuLuan/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TieuLuan.Models;
 
 namespace TieuLuan.Controllers
 {
     public class HomeController : Controller
     {
+        CT464Entities db = new CT464Entities();
+
         public ActionResult Index()
         {
+            var List = db.Products.Where(p => p.CategoryId == 1);
+            ViewBag.ListProduct = List;
+            var List1 = db.Products.OrderBy(p => p.ProductSold).Where(p => p.CategoryId == 1);
+            ViewBag.ListProduct1 = List1;
             return View();
         }
 
@@ -25,6 +32,10 @@ namespace TieuLuan.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult New_Products()
+        {
+                     return PartialView();
         }
     }
 }
